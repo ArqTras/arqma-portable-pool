d0f25d21cf01ec2559c513adf8c75d006ce96183
ArqTras <33489188+ArqTras@users.noreply.github.com>
2019-12-09 16:06:10 +0100

    Date

--- quasar.conf.js
+++ quasar.conf.js
@@ -208,7 +208,7 @@ module.exports = function (ctx) {
 
                 appId: "com.arqma.electron-wallet",
                 productName: "Arqma Electron Wallet",
-                copyright: "Copyright © 2018-2019 Arqma Project, 2018 Ryo/Loki Currency Project",
+                copyright: "Copyright © 2018-2020 Arqma Project, 2018-2020 Ryo/Loki Currency Project",
                 afterSign: "build/notarize.js",
 
                 // directories: {
